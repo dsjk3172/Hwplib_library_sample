b706e823b7015be0b9eb46bc253bb6d09ce21478
SiHyeon Lee <dsjk3172@naver.com>
2024-08-16 11:31:15 +0900

    2024_08_16 update

---
+++ src/main/java/hwp_create_main.java
@@ -0,0 +1,25 @@
+import kr.dogfoot.hwplib.object.HWPFile;
+import kr.dogfoot.hwplib.object.bodytext.Section;
+import kr.dogfoot.hwplib.object.bodytext.paragraph.Paragraph;
+import kr.dogfoot.hwplib.tool.blankfilemaker.BlankFileMaker;
+import kr.dogfoot.hwplib.writer.HWPWriter;
+
+public class hwp_create_main
+{
+	public static void main( String[] args )
+	{
+		try
+		{
+			HWPFile hwpFile = BlankFileMaker.make( );
+
+			Section s = hwpFile.getBodyText( ).getSectionList( ).get( 0 );
+			Paragraph firstParagraph = s.getParagraph( 0 );
+			firstParagraph.getText( ).addString( "이것은 추가된 문자열입니다." );
+			HWPWriter.toFile(hwpFile, "example.hwp" );
+		}
+		catch( Exception e )
+		{
+
+		}
+	}
+}
